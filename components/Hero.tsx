be6a5087f85e5c55dd7778c075ebf37997ed443a
mazharitanvir-hub <mazhari.tanvir@gmail.com>
2025-09-08 00:21:18 +0600

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -22,18 +22,42 @@ const Hero: React.FC<HeroProps> = ({ onStart }) => {
         >
           Upload Your Photos
         </button>
-      </div>
+      </section>
 
-      <div className="mt-16 sm:mt-24 max-w-4xl mx-auto">
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 lg:gap-8">
-            <BeforeAfterCard beforeSrc="https://picsum.photos/seed/before1/400/400" afterSrc="https://picsum.photos/seed/after1/400/400" />
-            <BeforeAfterCard beforeSrc="https://picsum.photos/seed/before2/400/400" afterSrc="https://picsum.photos/seed/after2/400/400" />
-            <div className="hidden lg:block">
-                <BeforeAfterCard beforeSrc="https://picsum.photos/seed/before3/400/400" afterSrc="https://picsum.photos/seed/after3/400/400" />
-            </div>
-        </div>
-      </div>
-    </div>
+      <section className="py-12">
+        <h3 className="text-3xl font-bold text-brand-gray-900 mb-8">From Casual to Corporate</h3>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6">
+  <div className="overflow-hidden rounded-lg border-4 border-white shadow-lg">
+    <img
+      src="https://lh4.googleusercontent.com/iiWKRS-oHG3enPXuptFzKlsqf93srfRpFCZAmx52L2Chu9WvJ-Yqq7LReDImB66FXr0e_04EfHwI8u3AqH-qaOcKrP2SpotPUASV8tX1mC9p2zLWN6TEmyQKzCehweWru9X9gJ8FS1A=w1280"
+      alt="Photo 1"
+      className="w-full h-full object-cover"
+    />
+  </div>
+  <div className="overflow-hidden rounded-lg border-4 border-white shadow-lg">
+    <img
+      src="https://lh6.googleusercontent.com/Bf2eCReODmtngw4CD96QoUf20ChSfOez54OrAF5Tq2ftE-b3bLtF2yMvhLdCPUXV9i0i9VoqkMNDhPlPtOXPZWShrV1-XeM7Y9T77weCG-r1GSxQA7H95jKdRW0zvvv8uagjFn7YIB8=w1280"
+      alt="Photo 2"
+      className="w-full h-full object-cover"
+    />
+  </div>
+  <div className="overflow-hidden rounded-lg border-4 border-white shadow-lg">
+    <img
+      src="https://lh5.googleusercontent.com/4fKOAXeEoZRVAfWj2kCPr7p6SqzSuQQSkmdqO2IfWbWu4HisufC7UV2PAR2uvrH9PWBk0bWLlVuMWG8tGjDHRjUIsDBNnYXIFMOhO1voZlBFJaYCYfphQ1ZtN0Qqle38lzDARcXzDJ8=w1280"
+      alt="Photo 3"
+      className="w-full h-full object-cover"
+    />
+  </div>
+  <div className="overflow-hidden rounded-lg border-4 border-white shadow-lg">
+    <img
+      src="https://lh3.googleusercontent.com/GG56Zq5_qDwAa7_N7qHbgEO1gMIKguMMyc3N72bSy1cERlfYWs0X51gKAvnLxYTQvzISo8F0kHodIXkLqkwtlduRDDtVHc7XXArbZC3bzAW9aDx10DzYG4OmsvHfisXMKLSEMtEFvgk=w1280"
+      alt="Photo 4"
+      className="w-full h-full object-cover"
+    />
+  </div>
+</div>
+
+      </section>
   );
 };
 
